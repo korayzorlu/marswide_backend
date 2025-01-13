53219fefc37bb31039199e5780ae18f6f2785b0f
korayzorlu <koray.zorlu@yahoo.com>
2025-01-13 18:03:36 +0300

    .

--- users/models.py
+++ users/models.py
@@ -15,19 +15,19 @@ from django.contrib.auth.models import User
 #     USERNAME_FIELD = 'email'
 #     REQUIRED_FIELDS = ['username','first_name','last_name']
 
-class Profile(models.Model):
-    """
-    This model represents the profile info of Internal Personals.
-    """
+# class Profile(models.Model):
+#     """
+#     This model represents the profile info of Internal Personals.
+#     """
     
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, blank=True)
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, blank=True)
 
-    THEME_CHOICES = (('dark', ('Dark')), ('light', ('Light')))
-    theme = models.CharField(_("Theme"), max_length=25, default='light', choices=THEME_CHOICES, blank=True, null=True)
+#     THEME_CHOICES = (('dark', ('Dark')), ('light', ('Light')))
+#     theme = models.CharField(_("Theme"), max_length=25, default='light', choices=THEME_CHOICES, blank=True, null=True)
     
     
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+#     created_at = models.DateTimeField(auto_now_add=True, null=True)
+#     updated_at = models.DateTimeField(auto_now=True, null=True)
 
-    def __str__(self):
-        return f"{self.user} | {self.user.get_full_name()}"
+#     def __str__(self):
+#         return f"{self.user} | {self.user.get_full_name()}"
