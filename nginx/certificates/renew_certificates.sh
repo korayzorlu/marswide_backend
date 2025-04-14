872e612ca2e8d88a81a24989240e06de4a8690f2
korayzorlu <koray.zorlu@yahoo.com>
2025-04-15 00:06:39 +0300

    14042025 fix: fixed nginx

---
+++ nginx/certificates/renew_certificates.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Log dosyasını tanımla
+LOGFILE="/var/log/letsencrypt/renew.log"
+
+# Certbot yenileme komutunu çalıştır
+docker run --rm \
+  -v "$(pwd)/nginx/certbot-www:/var/www/certbot" \
+  -v "$(pwd)/nginx/certificates:/etc/letsencrypt" \
+  certbot/certbot renew --webroot --webroot-path=/var/www/certbot > "$LOGFILE" 2>&1
+
+# Log dosyasını kontrol et
+if grep -q "Congratulations, all renewals succeeded" "$LOGFILE" || grep -q "new certificate deployed" "$LOGFILE"; then
+    echo "Sertifikalar yenilendi. NGINX yeniden başlatılıyor..."
+    docker restart nginx
+else
+    echo "Sertifikalar zaten güncel. NGINX yeniden başlatılmadı."
+fi
